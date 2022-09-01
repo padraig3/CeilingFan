79600e4b22664b56cca90f408be2b413186c76ee
patrick <ptrainor@gmail.com>
2022-09-01 18:53:52 -0400

    initial import

--- src/main/java/org/ptrainor/CeilingFan.java
+++ src/main/java/org/ptrainor/CeilingFan.java
@@ -1,2 +1,87 @@
-package org.ptrainor;public class CeilingFan {
+package org.ptrainor;
+
+public class CeilingFan {
+
+    // number of speed settings apart from the 'off' setting. must be > 0
+    public final static int NUM_SPEEDS = 3;
+    // speed of fan when initially instantiated. must be in range 0..NUM_SPEEDS
+    public final static int INITIAL_SPEED = 0;
+    // direction of fan when initially instantiated. must be either -1 or +1
+    public final static int INITIAL_DIRECTION = 1;
+
+    /*
+     * speed of fan is integer in inclusive range 0..NUM_SPEEDS
+     * where 0 = at rest / no speed
+     * non-zero values are speeds proportional to integer value.
+     * NUM_SPEEDS value is fastest, NUM_SPEEDS - 1 slower than NUM_SPEEDS, etc
+     */
+    private int speed;
+    /*
+     * direction of fan: +1 = forward, -1 = backwards
+     */
+    private int direction;
+
+    public CeilingFan() {
+        if (NUM_SPEEDS <= 0) {
+            throw new RuntimeException("Class attribute NUM_SPEEDS must be greater than zero.");
+        }
+        if (INITIAL_SPEED < 0 || INITIAL_SPEED > NUM_SPEEDS) {
+            throw new RuntimeException("Class attribute INITIAL_SPEED must not be negative and <= NUM_SPEEDS.");
+        }
+        speed = INITIAL_SPEED;
+
+        if (INITIAL_DIRECTION != -1 && INITIAL_DIRECTION != 1) {
+            throw new RuntimeException("Class attribute INITIAL_DIRECTION must be either 1 or -1.");
+        }
+        direction = INITIAL_DIRECTION;
+    }
+
+    /**
+     * Increments the fan speed. If speed is currently at max speed will set the speed to zero
+     */
+    public void pullSpeedCord() {
+        speed++;
+        // this modulus operation will set speed to zero if was at maximum speed before increment
+        speed = speed % (NUM_SPEEDS + 1);
+        // FYI could implement this method with one line "++speed % (NUM_SPEEDS + 1)"
+        // but is less readable / intuitive IMO
+    }
+
+    /**
+     * Reverses the fan direction
+     */
+    public void pullReverseCord() {
+        direction *= -1;
+    }
+
+    /**
+     * @return non-negative integer where 0 means no speed and non-zero values
+     * indicate speeds proportional to their value.
+     * For example 3 is faster than 2, 2 is faster than 1
+     */
+    public final int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * @return integer either -1 or +1 where:
+     * +1 - forward direction
+     * -1 - reverse direction
+     */
+    public final int getDirection() {
+        return direction;
+    }
+
+    /**
+     * @return integer in range -NUM_SPEEDS,.. -1, 0, +1, ..NUM_SPEEDS where:
+     * 0 - no direction or speed / fan at rest
+     * positive values - speed values where fan is moving in forward direction
+     * negative values - speed values where fan is moving in reverse direction
+     *
+     * As described above, speeds are proportional to their absolute value. For example,
+     * abs(-3) is faster than 2, 2 is faster than abs(-1)
+     */
+    public final int getSpeedDirection() {
+        return speed * direction;
+    }
 }
--- src/test/java/org/ptrainor/CeilingFanTest.java
+++ src/test/java/org/ptrainor/CeilingFanTest.java
@@ -1,4 +1,90 @@
+package org.ptrainor;
+
 import junit.framework.TestCase;
+
 public class CeilingFanTest extends TestCase {
-  
+
+    /**
+     * Checks expected vs actual direction, speed and direction * speed. Invoking tests will fail on
+     * any failed assert statements.
+     *
+     * @param fan the CeilingFan instance
+     * @param expectedDirection expected direction
+     * @param expectedSpeed expected speed
+     */
+    private void validate(final CeilingFan fan, final int expectedDirection, final int expectedSpeed) {
+        assertEquals(expectedDirection, fan.getDirection());
+        assertEquals(expectedSpeed, fan.getSpeed());
+        assertEquals(expectedDirection * expectedSpeed, fan.getSpeedDirection());
+    }
+
+    /**
+     * Tests that initially created fan has expected speed and direction
+     */
+    public void testInitial() {
+        final int expectedDirection = CeilingFan.INITIAL_DIRECTION;
+        final int expectedSpeed = CeilingFan.INITIAL_SPEED;
+        CeilingFan fan = new CeilingFan();
+        validate(fan, expectedDirection, expectedSpeed);
+    }
+
+    /**
+     * Tests expectations when speed cord pulled once
+     */
+    public void testPullSpeed() {
+        final int expectedDirection = CeilingFan.INITIAL_DIRECTION;
+        final int expectedSpeed = (CeilingFan.INITIAL_SPEED + 1) % (CeilingFan.NUM_SPEEDS + 1);
+        CeilingFan fan = new CeilingFan();
+        fan.pullSpeedCord();
+        validate(fan, expectedDirection, expectedSpeed);
+    }
+
+    /**
+     * Tests expectations when reverse cord pulled once
+     */
+    public void testPullReverse() {
+        final int expectedDirection = -CeilingFan.INITIAL_DIRECTION;
+        final int expectedSpeed = CeilingFan.INITIAL_SPEED;
+        CeilingFan fan = new CeilingFan();
+        fan.pullReverseCord();
+        validate(fan, expectedDirection, expectedSpeed);
+    }
+
+    /**
+     * Tests expectations when speed cord pulled once and reverse cord pulled once
+     */
+    public void testPullSpeedReverse() {
+        final int expectedDirection = -CeilingFan.INITIAL_DIRECTION;
+        final int expectedSpeed = (CeilingFan.INITIAL_SPEED + 1) % (CeilingFan.NUM_SPEEDS + 1);
+        CeilingFan fan = new CeilingFan();
+        fan.pullSpeedCord();
+        fan.pullReverseCord();
+        validate(fan, expectedDirection, expectedSpeed);
+    }
+
+    /**
+     * Tests expectations when speed cord pulled twice
+     */
+    public void testPullSpeed2() {
+        final int expectedDirection = CeilingFan.INITIAL_DIRECTION;
+        final int expectedSpeed = (CeilingFan.INITIAL_SPEED + 2) % (CeilingFan.NUM_SPEEDS + 1);
+        CeilingFan fan = new CeilingFan();
+        for (int i = 0; i < 2; i++) {
+            fan.pullSpeedCord();
+        }
+        validate(fan, expectedDirection, expectedSpeed);
+    }
+
+    /**
+     * Tests expectations when speed cord pulled 4x
+     */
+    public void testPullSpeed4() {
+        final int expectedDirection = CeilingFan.INITIAL_DIRECTION;
+        final int expectedSpeed = (CeilingFan.INITIAL_SPEED + 4) % (CeilingFan.NUM_SPEEDS + 1);
+        CeilingFan fan = new CeilingFan();
+        for (int i = 0; i < 4; i++) {
+            fan.pullSpeedCord();
+        }
+        validate(fan, expectedDirection, expectedSpeed);
+    }
 }
